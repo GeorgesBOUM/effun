5298876c39d8bec1c0e93d692049f7b12037fed8
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-01 05:07:55 +0200

    Create EffunApplication.java

---
+++ src/main/java/org/georges/effun/EffunApplication.java
@@ -0,0 +1,13 @@
+package org.georges.effun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffunApplication.class, args);
+	}
+
+}
