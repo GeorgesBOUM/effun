8808e3ca0ffb2bd67dbf4a104c867c1f7a24eeb8
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-01 05:07:58 +0200

    Create EffunApplicationTests.java

---
+++ src/test/java/org/georges/effun/EffunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.georges.effun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
