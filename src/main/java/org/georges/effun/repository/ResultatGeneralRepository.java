6cd0e53dd87aba4e10520fc462dc814aa490a94b
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-03 11:55:11 +0200

    Create ResultatGeneralRepository.java

---
+++ src/main/java/org/georges/effun/repository/ResultatGeneralRepository.java
@@ -0,0 +1,8 @@
+package org.georges.effun.repository;
+
+import org.georges.effun.model.ResultatGeneral;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResultatGeneralRepository extends CrudRepository<ResultatGeneral, Long> {
+
+}
