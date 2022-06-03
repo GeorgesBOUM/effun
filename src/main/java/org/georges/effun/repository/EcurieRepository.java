43f90d2348ff38fa21853092b7e850e85da1e355
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-03 11:55:15 +0200

    Create EcurieRepository.java

---
+++ src/main/java/org/georges/effun/repository/EcurieRepository.java
@@ -0,0 +1,8 @@
+package org.georges.effun.repository;
+
+import org.georges.effun.model.Ecurie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EcurieRepository extends CrudRepository<Ecurie, Long> {
+
+}
