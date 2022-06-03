839fc97c5de19c8f2a4479597d6e1b2313a6d637
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-03 11:55:06 +0200

    Create ResultatPiloteRepository.java

---
+++ src/main/java/org/georges/effun/repository/ResultatPiloteRepository.java
@@ -0,0 +1,8 @@
+package org.georges.effun.repository;
+
+import org.georges.effun.model.ResultatPilote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResultatPiloteRepository extends CrudRepository<ResultatPilote, Integer> {
+	
+}
