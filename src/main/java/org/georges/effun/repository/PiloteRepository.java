1a860c84cdd9eb896d9bf507c09d59166b2fabc9
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-03 11:54:52 +0200

    Create PiloteRepository.java

---
+++ src/main/java/org/georges/effun/repository/PiloteRepository.java
@@ -0,0 +1,8 @@
+package org.georges.effun.repository;
+
+import org.georges.effun.model.Pilote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PiloteRepository extends CrudRepository<Pilote, Integer> {
+
+}
