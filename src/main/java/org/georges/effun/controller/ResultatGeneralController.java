5d56c0eed114c5c427c22e7ddd5f753830057138
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-04 15:37:58 +0200

    Create ResultatGeneralController.java

---
+++ src/main/java/org/georges/effun/controller/ResultatGeneralController.java
@@ -0,0 +1,41 @@
+package org.georges.effun.controller;
+
+import java.util.ArrayList;
+
+import org.georges.effun.model.Pilote;
+import org.georges.effun.model.ResultatGeneral;
+import org.georges.effun.model.ResultatPilote;
+import org.georges.effun.service.ResultatGeneralService;
+import org.georges.effun.utils.ComparaisonMeilleurTemps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResultatGeneralController {
+
+    @Autowired
+    private ResultatGeneralService rgs;
+
+    @GetMapping("/standings/pilotes/{annee}")
+    public void getClassementPilotesParAnnee(@PathVariable int annee) {
+	/**
+	 * Le type de retour ne sera évidemment pas void. C'est pour éviter les erreurs syntaxiques
+	 * 
+	 * Je veux le classement des pilotes par années => une sorted list ?
+	 * Il faut calculer les points par pilote (distribués selon la place du pilote)
+	 * Plus un point bonus pour le meilleur tour si dans les 10 premiers
+	 * Si même nombre de points, avantage à la meilleure place
+	 * Si même nombre de points et même place, avantage aux plus de victoires
+	 * Chaque pilote devrait donc embarquer un compteur de point
+	 * => Il faut donc un résultat général par pilote et par année
+	 */
+	
+	rgs.getResultatGeneralParAnnee(annee)
+	   .get() // peut être catcher le NoSuchElementException ?
+	   .getListeResultatsTousPilotes()
+	   .stream(); // stream en chantier
+    }    
+    
+}
