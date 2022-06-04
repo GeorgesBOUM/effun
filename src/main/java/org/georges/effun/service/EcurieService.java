dca6c9d5dac8717b96f720c085eb5f195f68bcc4
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-04 10:44:25 +0200

    Create EcurieService.java

---
+++ src/main/java/org/georges/effun/service/EcurieService.java
@@ -0,0 +1,24 @@
+package org.georges.effun.service;
+
+import java.util.ArrayList;
+
+import org.georges.effun.model.Ecurie;
+import org.georges.effun.repository.EcurieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+
+@Service
+public class EcurieService {
+    @Autowired
+    EcurieRepository er;
+    
+    public ArrayList<Ecurie> getAllEcuries() {
+	return Lists.newArrayList(er.findAll());
+    }
+    
+    public Ecurie ajoutEcurie(Ecurie e) {
+	return er.save(e);
+    }
+}
