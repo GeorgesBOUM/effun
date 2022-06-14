f288802a6bbfc462ca92ed04e35fd11259c10971
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-06-14 06:50:34 +0200

    create getPiloteDetails(id)

--- src/main/java/org/georges/effun/controller/PiloteController.java
+++ src/main/java/org/georges/effun/controller/PiloteController.java
@@ -1,9 +1,12 @@
 package org.georges.effun.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.georges.effun.model.Pilote;
+import org.georges.effun.model.PiloteDetail;
 import org.georges.effun.service.GrandPrixService;
 import org.georges.effun.service.PiloteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,8 @@ public class PiloteController {
     private PiloteService ps;
     @Autowired
     private GrandPrixService rgs;
+    @Autowired
+    private GrandPrixController gpc;
 
     @GetMapping("/pilotes")
     public List<Pilote> getAllPilotes() {
@@ -38,8 +43,8 @@ public class PiloteController {
 	return ps.getPiloteById(id);
     }
 
-    @GetMapping("/pilotes:{piloteId}/victoire")
-    public Long nombreVictoiresPilote(@PathVariable Integer id) {
+    @GetMapping("/pilotes/{piloteId}/victoire")
+    public Long getNombreVictoiresPilote(@PathVariable Integer id) {
 	return rgs.getAllResultatsGeneraux()
 		  .stream()
 		  .filter(rgl -> rgl.getListeResultatsTousPilotes()
@@ -47,5 +52,32 @@ public class PiloteController {
 			            .anyMatch(rpl -> rpl.getPilote().getId().equals(id) && rpl.getPlace().equals(1)))
 		  .count();
 
+    }
+    
+    @GetMapping("/pilotes/{piloteId}/details")
+    public Optional<PiloteDetail> getPiloteDetails(@PathVariable Integer id){
+	/**
+	 * On peut getPilote à partir de l'id
+	 * Une fois qu'on a le pilote, on va dans la map et on récupère ses points
+	 * On pourra ensuite compter les points en appelant la méthode getNombreVictoires
+	 * */
+	int annee = LocalDate.now().getYear();
+	Map<Pilote, Integer> classementPilotes = gpc.getClassementPilotesParAnnee(annee);
+	Pilote p = ps.getPiloteById(id).get();
+	PiloteDetail pd = new PiloteDetail();
+	int place;
+	pd.setPilote(p);
+	pd.setPoints(classementPilotes.get(p));
+	place = rgs.getGrandPrixParAnnee(annee)
+		   .get()
+		   .getListeResultatsTousPilotes()
+		   .stream()
+		   .filter(rp -> rp.getPilote().equals(p))
+		   .findAny()
+		   .get()
+		   .getPlace();
+	pd.setPlace(place);
+	pd.setNombreVictoires(this.getNombreVictoiresPilote(id));
+	return Optional.ofNullable(pd);
     }
 }
